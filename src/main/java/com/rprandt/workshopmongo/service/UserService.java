7430bb39c2b4226f620a239574745989e36d808c
Richard Prandt <rprandt.contato@gmail.com>
2022-01-14 19:52:35 -0300

    delecao de usuario com delete

--- src/main/java/com/rprandt/workshopmongo/service/UserService.java
+++ src/main/java/com/rprandt/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rprandt.workshopmongo.domain.User;
+import com.rprandt.workshopmongo.dto.UserDTO;
 import com.rprandt.workshopmongo.repository.UserRepository;
 import com.rprandt.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -23,5 +24,18 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);;
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 }
