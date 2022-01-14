d1c6ba25146ef65ea66eccaa825c56bee3c04652
Richard Prandt <rprandt.contato@gmail.com>
2022-01-14 17:06:12 -0300

    obtendo um usuario por id

--- src/main/java/com/rprandt/workshopmongo/service/UserService.java
+++ src/main/java/com/rprandt/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.rprandt.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rprandt.workshopmongo.domain.User;
 import com.rprandt.workshopmongo.repository.UserRepository;
+import com.rprandt.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,8 @@ public class UserService {
 		return userRepository.findAll();
 	}
 	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+	}
 }
