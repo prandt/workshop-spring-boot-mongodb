8d016cdc19c6c2bf51f7d17490af6e85dbd4fbad
Richard Prandt <rprandt.contato@gmail.com>
2022-01-15 11:12:51 -0300

    consulta simples com query methods

--- src/main/java/com/rprandt/workshopmongo/service/PostService.java
+++ src/main/java/com/rprandt/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.rprandt.workshopmongo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
 	}
 	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
 	
 }
