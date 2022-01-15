aa72afdacd840f4b0a5238b3193b7edb0bc4d09c
Richard Prandt <rprandt.contato@gmail.com>
2022-01-15 10:45:01 -0300

    obtendo post por id

---
+++ src/main/java/com/rprandt/workshopmongo/service/PostService.java
@@ -0,0 +1,25 @@
+package com.rprandt.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rprandt.workshopmongo.domain.Post;
+import com.rprandt.workshopmongo.repository.PostRepository;
+import com.rprandt.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+	}
+	
+	
+}
